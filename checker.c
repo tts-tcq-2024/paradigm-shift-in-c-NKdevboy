9e8131f78e3b8a1e00defbf21a905730880192a3
NKdevboy <nithishkumarramup@gmail.com>
2024-06-24 20:22:04 +0530

    Update checker.c -v6 pure function

--- checker.c
+++ checker.c
@@ -2,53 +2,41 @@
 #include <assert.h>
 
 
-int batteryTempCheck(float temperature)
+void batteryTempCheck(float temperature,int * out)
 {
     if(temperature < 0 || temperature > 45)
     {
         printf("Temperature out of range!\n");
-        return 0;
-    }
-    else 
-    {
-        return 1;
+        *out &=0;
     }
 }
 
-int batterySocCheck(float soc)
+void batterySocCheck(float soc,int * out)
 {
     if(soc < 20 || soc > 80)
     {
         printf("State of Charge out of range!\n");
-        return 0;
-     }
-    else 
-     {
-        return 1;
+        *out &=0;
      }
 }
 
-int batterychargeRateCheck(float chargeRate)
+void batterychargeRateCheck(float chargeRate,int * out)
 {
     if(chargeRate > 0.8) 
     {
         printf("Charge Rate out of range!\n");
-        return 0;
+        *out &=0;
     }
-    else 
-     {
-        return 1;
-     }
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
- // int returnVal = 1;
- // returnVal = batteryTempCheck(temperature);
- // returnVal = batterySocCheck(soc);
- // returnVal = batterychargeRateCheck(chargeRate);
- // return returnVal;
- return (batteryTempCheck(temperature) && batterySocCheck(soc) && batterychargeRateCheck(chargeRate) );
+  int returnVal = 1;
+  batteryTempCheck(temperature,&returnVal);
+  batterySocCheck(soc,&returnVal);
+  batterychargeRateCheck(chargeRate,&returnVal);
+  return returnVal;
+ //return (batteryTempCheck(temperature) && batterySocCheck(soc) && batterychargeRateCheck(chargeRate) );
 }
 
 int main() {
